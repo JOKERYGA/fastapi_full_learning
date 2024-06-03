dc99892479ca17c8184c16b5eb1c9626ca470373
Vladislav Glukhovskiy <vlad.gluhovskiy_18376@mail.ru>
2024-06-03 17:26:01 +0700

    Установил библиотеки: sqlalchemy alembic psycopg2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from typing import List
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 from pydantic import BaseModel, Field
 import uvicorn
 from fastapi import FastAPI
@@ -10,10 +12,33 @@ app = FastAPI(
 
 fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2024-01-01T00:00:00", "type_degree": "expert"}
+        ]},
 ]
 
 
-@app.get("/users/{user_id}")
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+    
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
