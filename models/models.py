1e4958a6d59def56953c27e544c778025d1649a1
Vladislav Glukhovskiy <vlad.gluhovskiy_18376@mail.ru>
2024-06-04 13:46:10 +0700

    Остановка на выборе метода регистрации

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 from typing import Any, Dict, List
 from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
-from sqlalchemy import ForeignKey, Integer, String, TIMESTAMP, JSON
+from sqlalchemy import Boolean, ForeignKey, Integer, String, TIMESTAMP, JSON
 
 
 class Base(DeclarativeBase):
@@ -9,7 +9,7 @@ class Base(DeclarativeBase):
 
 
 class Role(Base):
-    __tablename__ = "roles"
+    __tablename__ = "role"
     
     id: Mapped[int] = mapped_column(primary_key=True,)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
@@ -17,11 +17,20 @@ class Role(Base):
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
     
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String, nullable=False)
     username: Mapped[str] = mapped_column(String, nullable=False)
-    password: Mapped[str] = mapped_column(String, nullable=False)
-    registered_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=datetime.now(timezone.utc))
-    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('roles.id'))
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    registered_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), default=datetime.now(timezone.utc)
+        )
+    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('role.id'))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
